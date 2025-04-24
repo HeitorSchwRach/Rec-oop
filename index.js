153d68717a9c68f6e0883df5d45dc284b3180de9
HeitorSrDev <heitor_rachadel@estudante.sesisenai.org.br>
2025-04-24 07:48:20 -0300

    Criação das classes Guerreiro, Mago e Arqueiro herdando de Personagem

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 class Personagem{
-    constructor(vida, forca, mana){
+    constructor(vida, forca, mana, stamina, precisao){
         this.vida = vida
         this.forca = forca
         this.mana = mana
+        this.stamina = stamina
+        this.precisao = precisao
     }
 }
 
 class Guerreiro extends Personagem{
-
+    constructor(vida, forca,stamina){
+        super(vida, forca, stamina)
+    }
 }
 
 class Mago extends Personagem{
+    constructor(vida, mana, forca){
+        super(vida, mana, forca)
+    }
 
 }
 
 class Arqueiro extends Personagem{
-    
+    constructor(vida, forca, stamina, precisao){
+        super(vida, forca, stamina, precisao)
+    }
 }
