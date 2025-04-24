427a6102fd14c8cfb5e4b209a2f2626a1049272a
HeitorSrDev <heitor_rachadel@estudante.sesisenai.org.br>
2025-04-24 09:10:16 -0300

    alteracoes nas funcoes, criacao de personagem e classe jogo

--- index.js
+++ index.js
@@ -13,81 +13,50 @@ class Personagem {
         this.#precisao = precisao;
     }
 
-
-    get vida() {
-        return this.#vida;
-    }
-
-    get forca() {
-        return this.#forca;
-    }
-
-    get mana() {
-        return this.#mana;
-    }
-
-    get stamina() {
-        return this.#stamina;
-    }
-
-    get precisao() {
-        return this.#precisao;
-    }
-
+    get vida() { return this.#vida; }
+    get forca() { return this.#forca; }
+    get mana() { return this.#mana; }
+    get stamina() { return this.#stamina; }
+    get precisao() { return this.#precisao; }
 
     set vida(valor) {
-        if (valor >= 0) {
-            this.#vida = valor;
-        } else {
-            console.log('Vida não pode ser menor que 0');
-        }
+        if (valor >= 0) this.#vida = valor;
+        else console.log('Vida não pode ser menor que 0');
     }
-
     set forca(valor) {
-        if (valor >= 0) {
-            this.#forca = valor;
-        } else {
-            console.log('Força não pode ser menor que 0');
-        }
+        if (valor >= 0) this.#forca = valor;
+        else console.log('Força não pode ser menor que 0');
     }
-
     set mana(valor) {
-        if (valor >= 0) {
-            this.#mana = valor;
-        } else {
-            console.log('Mana não pode ser menor que 0');
-        }
+        if (valor >= 0) this.#mana = valor;
+        else console.log('Mana não pode ser menor que 0');
     }
-
     set stamina(valor) {
-        if (valor >= 0) {
-            this.#stamina = valor;
-        } else {
-            console.log('Stamina não pode ser menor que 0');
-        }
+        if (valor >= 0) this.#stamina = valor;
+        else console.log('Stamina não pode ser menor que 0');
     }
-
     set precisao(valor) {
-        if (valor >= 0) {
-            this.#precisao = valor;
+        if (valor >= 0) this.#precisao = valor;
+        else console.log('Precisão não pode ser menor que 0');
+    }
+
+    atacar(inimigo) {
+        inimigo.vida -= this.#forca;
+    }
+
+    defender(inimigo) {
+        if (this.#vida > 0) {
+            inimigo.atacar(this);
+        }
+    }
+
+    usarMagia(inimigo) {
+        if (this.#mana >= 5) {
+            inimigo.vida -= this.#forca;
+            this.#mana -= 5;
         } else {
-            console.log('Precisão não pode ser menor que 0');
+            console.log('Mana insuficiente');
         }
-    }
-
-    atacar(Inimigo) {
-        Inimigo.vida -= this.#forca;
-    }
-
-    defender(Inimigo) {
-        if (this.#vida) {
-            Inimigo.atacar(this);
-        }
-    }
-
-    usarMagia(Inimigo) {
-        Inimigo.vida -= this.#forca;
-        this.#mana -= 5;
     }
 }
 
@@ -96,18 +65,15 @@ class Guerreiro extends Personagem {
         super(vida, forca, 0, stamina, 0);
     }
 
-    atacar(Inimigo) {
+    atacar(inimigo) {
         if (this.stamina >= 3) {
-            Inimigo.vida -= this.forca;
+            inimigo.vida -= this.forca;
             this.stamina -= 3;
+        } else {
+            console.log('Guerreiro está cansado demais para atacar.');
         }
     }
-    cansado(){
-        if(this.stamina = 0){
-            Guerreiro.atacar = false
-            console.log("Guerreiro sem stamina")
-        }
-    }
+
 }
 
 class Mago extends Personagem {
@@ -115,18 +81,19 @@ class Mago extends Personagem {
         super(vida, forca, mana, 0, 0);
     }
 
-    usarMagia(Inimigo) {
+    usarMagia(inimigo) {
         if (this.mana >= 5) {
-            Inimigo.vida -= this.forca;
+            inimigo.vida -= this.forca;
             this.mana -= 5;
+        } else {
+            console.log('Mana insuficiente para usar magia.');
         }
     }
-    cansado() {
-        if (this.mana = 0) {
-            Mago.usarMagia = false
-            console.log("Mana insuficiente")
-        }
+
+    atacar(inimigo) {
+        this.usarMagia(inimigo);
     }
+
 }
 
 class Arqueiro extends Personagem {
@@ -134,33 +101,99 @@ class Arqueiro extends Personagem {
         super(vida, forca, 0, stamina, precisao);
     }
 
-    atacar(Inimigo) {
+    atacar(inimigo) {
         if (this.stamina >= 2) {
-            Inimigo.vida -= this.forca;
+            inimigo.vida -= this.forca;
             this.stamina -= 2;
+        } else {
+            console.log('Arqueiro sem stamina suficiente para atacar.');
         }
     }
 
     cansar() {
         if (this.stamina >= 2) {
             this.stamina -= 2;
-            this.precisao -= 1;
+            this.precisao -= 15;
         }
     }
-    cansado(){
-        if(this.stamina = 0){
-            Arqueiro.atacar = false
-            console.log("Sem stamina suficiente para atacar")
-        }
-    }
+
+
 }
 
-class InimigoG extends Personagem {
+class InimigoG extends Guerreiro {
+    constructor(vida, forca, stamina) {
+        super(vida, forca, 0, stamina, 0)
+    }
+
+    atacar(inimigo) {
+        if (this.stamina >= 3) {
+            inimigo.vida -= this.forca;
+            this.stamina -= 3;
+        } else {
+            console.log('InimigoG está cansado demais para atacar.');
+        }
+    }
+
+}
+
+class InimigoA extends Arqueiro {
     constructor(vida, forca, stamina, precisao) {
         super(vida, forca, 0, stamina, precisao);
     }
+
+    atacar(inimigo) {
+        if (this.stamina >= 2) {
+            inimigo.vida -= this.forca;
+            this.stamina -= 2;
+        } else {
+            console.log('Arqueiro sem stamina suficiente para atacar.');
+        }
+    }
+
+    cansar() {
+        if (this.stamina >= 2) {
+            this.stamina -= 2;
+            this.precisao -= 15;
+        }
+    }
+
+
+}
+
+class inimigoM extends Mago {
+    constructor(vida, mana, forca) {
+        super(vida, forca, mana, 0, 0);
+    }
+
+    usarMagia(inimigo) {
+        if (this.mana >= 5) {
+            inimigo.vida -= this.forca;
+            this.mana -= 5;
+        } else {
+            console.log('Mana insuficiente para usar magia.');
+        }
+    }
+
+    atacar(inimigo) {
+        this.usarMagia(inimigo);
+    }
+
 }
 
 class Jogo {
+    constructor() {
+        this.personagens = [];
+    }
 
+    adicionarPersonagem(personagem) {
+        this.personagens.push(personagem);
+    }
 }
+const guerreiro = new Guerreiro(100, 20, 10);
+const mago = new Mago(80, 15, 18);
+const arqueiro = new Arqueiro(90, 12, 8, 100);
+const inimigoGuerreiro = new InimigoG(120, 10, 5);
+const inimigoMago = new inimigoM(70,10,16)
+const inimigoArqueiro = new InimigoA(80,10,7,80)
+const jogo = new Jogo();
+
